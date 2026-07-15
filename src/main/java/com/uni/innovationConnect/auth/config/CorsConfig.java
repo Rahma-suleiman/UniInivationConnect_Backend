7d56ddf518f84b15cdac04b7976f100b46520f8e
Rahma-suleiman <suleimanr166@gmail.com>
2026-07-15 10:43:55 +0300

    cors

---
+++ src/main/java/com/uni/innovationConnect/auth/config/CorsConfig.java
@@ -0,0 +1,79 @@
+package com.uni.innovationConnect.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+public class CorsConfig {
+
+
+    @Bean
+    public CorsFilter corsFilter(){
+
+
+        CorsConfiguration config = new CorsConfiguration();
+
+
+        config.addAllowedOrigin("http://localhost:5173");
+
+        config.addAllowedMethod("*");
+
+        config.addAllowedHeader("*");
+
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+
+        source.registerCorsConfiguration(
+                "/**",
+                config
+        );
+
+
+        return new CorsFilter(source);
+
+    }
+
+}
+// package com.uni.innovationConnect.auth.config;
+
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.*;
+
+// @Configuration
+// public class CorsConfig {
+
+
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer(){
+
+//         return new WebMvcConfigurer() {
+
+//             @Override
+//             public void addCorsMappings(
+//                     CorsRegistry registry) {
+
+//                 registry.addMapping("/**")
+//                         .allowedOrigins(
+//                             "http://localhost:5173"
+//                         )
+//                         .allowedMethods(
+//                             "GET",
+//                             "POST",
+//                             "PUT",
+//                             "DELETE"
+//                         )
+//                         .allowedHeaders("*");
+
+//             }
+
+//         };
+//     }
+// }
